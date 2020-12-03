da0b530bfb75f58e940f7c9ef398a4b40cf8904b
wyounis25 <github email address>
2020-12-03 12:53:27 -0600

    final touches

--- data/Note.js
+++ data/Note.js
@@ -1,8 +1,260 @@
 const notes = [
 	{
+		category: 'Neuroscience',
+		label: 'Neurons',
+		content: 'https://www.oxy.edu/sites/default/files/styles/banner_image/public/landing-page/banner-images/neuroscience_main_1440x800.jpg?itok=2w8RQm2a',
+		price: 500,
+		user: null,
+		experiences: [
+			{
+				review: 'it is what it is',
+				rating: 2,
+				user: null,
+				name: "bob"
+			},
+			{
+				review: 'excellent',
+				rating: 4,
+				user: null,
+				name: "bob"
+	
+			},
+			{
+				review: 'its Alright',
+				rating: 2,
+				user: null,
+				name: "bob"
+			
+			},
+			{
+				review: 'it is what it is',
+				rating: 2,
+				user: null,
+				name: "bob"
+			},
+			{
+				review: 'excellent',
+				rating: 4,
+				user: null,
+				name: "bob"
+	
+			},
+			{
+				review: 'its Alright',
+				rating: 2,
+				user: null,
+				name: "bob"
+			
+			},
+			{
+				review: 'it is what it is',
+				rating: 2,
+				user: null,
+				name: "bob"
+			},
+			{
+				review: 'excellent',
+				rating: 4,
+				user: null,
+				name: "bob"
+	
+			},
+			{
+				review: 'its Alright',
+				rating: 2,
+				user: null,
+				name: "bob"
+			
+			},
+			{
+				review: 'it is what it is',
+				rating: 2,
+				user: null,
+				name: "bob"
+			},
+			{
+				review: 'excellent',
+				rating: 4,
+				user: null,
+				name: "bob"
+	
+			},
+			{
+				review: 'its Alright',
+				rating: 2,
+				user: null,
+				name: "bob"
+			
+			}
+		]
+	},
+
+	{
+		category: 'Neuroscience',
+		label: 'Brain Scans',
+		content: 'https://neurosciencenews.com/files/2019/10/associated-memory-learning-neurosciencenews.jpg',
+		price: 250,
+		user: null,
+		experiences: [
+			{
+				review: 'Pretty  Notes',
+				rating: 1,
+				user: null,
+				name: "bob"
+			},
+			{
+				review: 'Pretty  Notes',
+				rating: 3,
+				user: null,
+				name: "bob"
+			},
+			{
+				review: 'Pretty  Notes',
+				rating: 1,
+				user: null,
+				name: "bob"
+			}
+		]
+	},
+	{
+		category: 'Neuroscience',
+		label: 'Brain & Behavior',
+		content: 'https://chiefexecutive.net/wp-content/uploads/2020/05/AdobeStock_217014433-1024x652.jpg',
+		price: 500,
+		user: null,
+		experiences: [
+			{
+				review: 'it is what it is',
+				rating: 2,
+				user: null,
+				name: "bob"
+			},
+			{
+				review: 'excellent',
+				rating: 4,
+				user: null,
+				name: "bob"
+	
+			},
+			{
+				review: 'its Alright',
+				rating: 2,
+				user: null,
+				name: "bob"
+			
+			},
+			{
+				review: 'it is what it is',
+				rating: 2,
+				user: null,
+				name: "bob"
+			},
+			{
+				review: 'excellent',
+				rating: 4,
+				user: null,
+				name: "bob"
+	
+			},
+			{
+				review: 'its Alright',
+				rating: 2,
+				user: null,
+				name: "bob"
+			
+			},
+			{
+				review: 'it is what it is',
+				rating: 2,
+				user: null,
+				name: "bob"
+			},
+			{
+				review: 'excellent',
+				rating: 4,
+				user: null,
+				name: "bob"
+	
+			},
+			{
+				review: 'its Alright',
+				rating: 2,
+				user: null,
+				name: "bob"
+			
+			},
+			{
+				review: 'it is what it is',
+				rating: 2,
+				user: null,
+				name: "bob"
+			},
+			{
+				review: 'excellent',
+				rating: 4,
+				user: null,
+				name: "bob"
+	
+			},
+			{
+				review: 'its Alright',
+				rating: 2,
+				user: null,
+				name: "bob"
+			
+			}
+		]
+	},
+
+	{
+		category: 'Enviornmental Science',
+		label: 'Soil Erosion and its impact',
+		content: 'https://blogs.egu.eu/geolog/files/2018/07/urban-gully-congo-1400x800.jpg',
+		price: 250,
+		user: null,
+		experiences: [
+			{
+				review: 'Pretty  Notes',
+				rating: 1,
+				user: null,
+				name: "bob"
+			},
+			{
+				review: 'Pretty  Notes',
+				rating: 3,
+				user: null,
+				name: "bob"
+			},
+			{
+				review: 'Pretty  Notes',
+				rating: 1,
+				user: null,
+				name: "bob"
+			},
+			{
+				review: 'Pretty  Notes',
+				rating: 1,
+				user: null,
+				name: "bob"
+			},
+			{
+				review: 'Pretty  Notes',
+				rating: 3,
+				user: null,
+				name: "bob"
+			},
+			{
+				review: 'Pretty  Notes',
+				rating: 1,
+				user: null,
+				name: "bob"
+			}
+		]
+	},
+	{
 		category: 'Science',
 		label: 'Neuroscience',
-		content: 'this is the content',
+		content: 'https://www.oxy.edu/sites/default/files/styles/banner_image/public/landing-page/banner-images/neuroscience_main_1440x800.jpg?itok=2w8RQm2a',
 		price: 500,
 		user: null,
 		experiences: [
@@ -30,9 +282,9 @@ const notes = [
 	},
 
 	{
-		category: 'Science',
-		label: 'Biology',
-		content: 'this is the another content',
+		category: 'Mechanical Engineering',
+		label: 'Flow Through States of Complex Engineering',
+		content: 'https://www.parraguezr.net/wp-content/uploads/2017/03/Information-Flow-paper-FinalPaperAsPublished.jpg',
 		price: 250,
 		user: null,
 		experiences: [
@@ -55,7 +307,235 @@ const notes = [
 				name: "bob"
 			}
 		]
+	},
+	{
+		category: 'Art',
+		label: 'Reflecting on Making Artistic Research',
+		content: 'https://i1.rgstatic.net/publication/277309257_On_Reflecting_and_Making_in_Artistic_Research/links/5566ef3808aeab77721cdcc1/largepreview.png',
+		price: 500,
+		user: null,
+		experiences: [
+			{
+				review: 'it is what it is',
+				rating: 2,
+				user: null,
+				name: "bob"
+			},
+			{
+				review: 'excellent',
+				rating: 4,
+				user: null,
+				name: "bob"
+	
+			},
+			{
+				review: 'its Alright',
+				rating: 2,
+				user: null,
+				name: "bob"
+			
+			}
+		]
+	},
+
+	{
+		category: 'Calculus',
+		label: 'Intergals',
+		content: 'https://i.redd.it/x19uc27r2xt21.jpg',
+		price: 250,
+		user: null,
+		experiences: [
+			{
+				review: 'Pretty  Notes',
+				rating: 1,
+				user: null,
+				name: "bob"
+			},
+			{
+				review: 'Pretty  Notes',
+				rating: 3,
+				user: null,
+				name: "bob"
+			},
+			{
+				review: 'Pretty  Notes',
+				rating: 1,
+				user: null,
+				name: "bob"
+			}
+		]
+	},
+	{
+		category: 'Calculus',
+		label: 'Derivative Practice',
+		content: 'https://media.cheggcdn.com/media%2F61d%2F61d2ebb0-25a2-4d2b-bd55-50e9fe5511d8%2Fimage',
+		price: 500,
+		user: null,
+		experiences: [
+			{
+				review: 'it is what it is',
+				rating: 2,
+				user: null,
+				name: "bob"
+			},
+			{
+				review: 'excellent',
+				rating: 4,
+				user: null,
+				name: "bob"
+	
+			},
+			{
+				review: 'its Alright',
+				rating: 2,
+				user: null,
+				name: "bob"
+			
+			}
+		]
+	},
+
+	{
+		category: 'Enviornmental Science',
+		label: 'Soil Erosion and its impact',
+		content: 'https://blogs.egu.eu/geolog/files/2018/07/urban-gully-congo-1400x800.jpg',
+		price: 250,
+		user: null,
+		experiences: [
+			{
+				review: 'Pretty  Notes',
+				rating: 1,
+				user: null,
+				name: "bob"
+			},
+			{
+				review: 'Pretty  Notes',
+				rating: 3,
+				user: null,
+				name: "bob"
+			},
+			{
+				review: 'Pretty  Notes',
+				rating: 1,
+				user: null,
+				name: "bob"
+			}
+		]
+	},
+	{
+		category: 'Economics',
+		label: 'What is Economics',
+		content: 'https://new-fullview-html.oneclass.com/Azp6YoGPbLXwYnKJxnvPVQJyE4wMarvl/low/bg1.png',
+		price: 500,
+		user: null,
+		experiences: [
+			{
+				review: 'it is what it is',
+				rating: 2,
+				user: null,
+				name: "bob"
+			},
+			{
+				review: 'excellent',
+				rating: 4,
+				user: null,
+				name: "bob"
+	
+			},
+			{
+				review: 'its Alright',
+				rating: 2,
+				user: null,
+				name: "bob"
+			
+			},
+			{
+				review: 'it is what it is',
+				rating: 2,
+				user: null,
+				name: "bob"
+			},
+			{
+				review: 'excellent',
+				rating: 4,
+				user: null,
+				name: "bob"
+	
+			},
+			{
+				review: 'its Alright',
+				rating: 2,
+				user: null,
+				name: "bob"
+			
+			}
+		]
+	},
+
+	{
+		category: 'Mechanical Engineering',
+		label: 'Flow Through States of Complex Engineering',
+		content: 'https://www.parraguezr.net/wp-content/uploads/2017/03/Information-Flow-paper-FinalPaperAsPublished.jpg',
+		price: 250,
+		user: null,
+		experiences: [
+			{
+				review: 'Pretty  Notes',
+				rating: 1,
+				user: null,
+				name: "bob"
+			},
+			{
+				review: 'Pretty  Notes',
+				rating: 3,
+				user: null,
+				name: "bob"
+			},
+			{
+				review: 'Pretty  Notes',
+				rating: 1,
+				user: null,
+				name: "bob"
+			},
+			{
+				review: 'Pretty  Notes',
+				rating: 1,
+				user: null,
+				name: "bob"
+			},
+			{
+				review: 'Pretty  Notes',
+				rating: 3,
+				user: null,
+				name: "bob"
+			},
+			{
+				review: 'Pretty  Notes',
+				rating: 1,
+				user: null,
+				name: "bob"
+			},
+			{
+				review: 'Pretty  Notes',
+				rating: 1,
+				user: null,
+				name: "bob"
+			},
+			{
+				review: 'Pretty  Notes',
+				rating: 3,
+				user: null,
+				name: "bob"
+			},
+			{
+				review: 'Pretty  Notes',
+				rating: 1,
+				user: null,
+				name: "bob"
+			}
+		]
 	}
+	
 ];
 
 export default notes;
--- data/User.js
+++ data/User.js
@@ -2,13 +2,13 @@ import bycrypt from 'bcryptjs'
 const users = [
     {
         name: "Bob",
-        username: "bobnum1",
+        username: "bob",
         password: bycrypt.hashSync('123456', 10),
         carts: []
     },
     {
-        name: "Bruce",
-        username: "batman1",
+        name: "mark",
+        username: "mark",
         password: bycrypt.hashSync('123456', 10),
         carts: []
     },
